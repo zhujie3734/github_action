9408cc7aaad4b1951e2e958cf6263e291ec215fc
zhujie3734 <jasonzhu1986@yahoo.com>
2025-02-11 22:24:42 +0000

    manually create s3 bucket first

--- main.tf
+++ main.tf
@@ -3,13 +3,10 @@ provider "aws" {
     region = "eu-north-1"
 }
 
-resource "aws_s3_bucket" "example" {
-    bucket = "my-terraform-bucket"
-}
 
 terraform {
     backend "s3" {
-      bucket = "my-terraform-bucket"
+      bucket = "my-terraform-bucket3734"
       key = "terraform/state.tfstate"
       region ="eu-north-1"
       encrypt = true
