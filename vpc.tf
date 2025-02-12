5ce4b893bab90863713b0056ce7ec34743a62b0e
zhujie3734 <jasonzhu1986@yahoo.com>
2025-02-12 23:43:09 +0000

    two different AZ

--- vpc.tf
+++ vpc.tf
@@ -13,6 +13,6 @@ resource "aws_subnet" "eks_subnet_1" {
 resource "aws_subnet" "eks_subnet_2" {
   vpc_id                  = aws_vpc.eks_vpc.id
   cidr_block              = "10.1.1.0/24"
-  availability_zone       = "eu-north-1a"
+  availability_zone       = "eu-north-1b"
   map_public_ip_on_launch = true
 }
