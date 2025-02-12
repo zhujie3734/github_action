15fbb1f7829bcb3cad456c5b68f2e6bf592402fc
zhujie3734 <jasonzhu1986@yahoo.com>
2025-02-12 22:56:21 +0000

    change vpc_id to id in vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -4,7 +4,7 @@ cidr_block = "10.1.0.0/16"
 }
 
 resource "aws_subnet" "eks_subnet" {
-    vpc_id = aws_vpc.eks_vpc.vpc_id
+    vpc_id = aws_vpc.eks_vpc.id
     cidr_block = "10.1.0.0/24"
     availability_zone = "eu-north-1a"
     map_public_ip_on_launch =true
