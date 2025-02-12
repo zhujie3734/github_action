5e58697194bb5b9a51352729a23bd943f87eaa9a
zhujie3734 <jasonzhu1986@yahoo.com>
2025-02-12 22:50:58 +0000

    fix role duplicate error

--- eks.tf
+++ eks.tf
@@ -1,6 +1,6 @@
 
-resource "aws_iam_role" "node_role" {
-    name = "eks-node-group-role"
+resource "aws_iam_role" "eks_role" {
+    name = "eks-cluster-role"
 
     assume_role_policy = jsonencode({
         Version = "2012-10-17"
@@ -15,9 +15,9 @@ resource "aws_iam_role" "node_role" {
 
 }
 
-resource "aws_iam_role_policy_attachment" "node_policy" {
-    policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-    role = aws_iam_role.node_role.name
+resource "aws_iam_role_policy_attachment" "eks_policy" {
+    policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+    role = aws_iam_role.eks_role.name
 }
 
 resource "aws_eks_cluster" "eks_cluster" {
