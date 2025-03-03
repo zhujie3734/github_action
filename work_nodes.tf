2a3b382214f00571b127f87187617b0fa25446d2
zhujie3734 <jasonzhu1986@yahoo.com>
2025-03-03 06:55:31 +0000

    create ssh key and modify node vpc to private

--- work_nodes.tf
+++ work_nodes.tf
@@ -1,3 +1,19 @@
+# Generate a new SSH key pair
+resource "tls_private_key" "eks_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+# Create an AWS key pair using the generated public key
+resource "aws_key_pair" "eks_key_pair" {
+  key_name   = "my-eks-key"  # Change this to your preferred key name
+  public_key = tls_private_key.eks_key.public_key_openssh
+
+  tags = {
+    Name = "EKS Key Pair"
+  }
+}
+
 
 resource "aws_iam_role" "eks_node_role" {
   name = "eks-node-group-role-1"
@@ -35,14 +51,14 @@ resource "aws_eks_node_group" "node_group" {
   cluster_name    = aws_eks_cluster.eks_cluster.name
   node_group_name = "minimal-node-group"
   node_role_arn   = aws_iam_role.eks_node_role.arn
-  subnet_ids      = [aws_subnet.public_subnet_1.id,
-                     aws_subnet.public_subnet_2.id]
+  subnet_ids      = [aws_subnet.private_subnet_1.id,
+                     aws_subnet.private_subnet_2.id]
   
   ami_type = "AL2_x86_64"
   instance_types =["t3.medium"]
 
   remote_access {
-    ec2_ssh_key = "my-key-pair"  # sshKey in remote-access can't be empty
+    ec2_ssh_key = aws_key_pair.eks_key_pair.key_name  # sshKey in remote-access can't be empty
     source_security_group_ids = [aws_security_group.eks_worker_sg.id] 
   }
   
